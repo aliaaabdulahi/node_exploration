5e879be1768a8d9707131abf3c58f7305772683f
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2020-04-27 01:15:56 -0400

    Update fs_demo.js

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -7,3 +7,35 @@ fs.mkdir(path.join(__dirname, "/test"), {}, function (err) {
     console.log('folder created...')
 }) // asynchronous -- takes in a callback. there is also a sync version.
 //this is creating the test folder in the reference folder
+
+
+// Create and write to file
+fs.writeFile(
+  path.join(__dirname, '/test', 'hello.txt'),
+  'Hello World!',
+  err => {
+    if (err) throw err;
+    console.log('File written to...');
+
+//  File append
+fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),' I love Node.js', err => {
+    if (err) throw err;
+    console.log('File written to...');
+});
+});
+
+// Read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+// Rename file
+fs.rename(
+  path.join(__dirname, '/test', 'hello.txt'),
+  path.join(__dirname, '/test', 'helloworld.txt'),
+  err => {
+    if (err) throw err;
+    console.log('File renamed...');
+  }
+);
